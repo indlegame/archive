d13d198458416fbac108ba68a18e5c6e28066b50
INDLE <101478722+indlegame@users.noreply.github.com>
2022-04-14 11:51:38 -0400

    Add files via upload

--- game_files/main.archive-v1.js
+++ game_files/main.archive-v1.js
@@ -251,9 +251,6 @@
       for (var s = 0, t = new Array(a); s < a; s++) t[s] = e[s];
       return t;
     }
-    var k = document.createElement("template");
-    k.innerHTML =
-      "\n<style>\n  :host {\n    display: inline-block;\n  }\n  .tile {\n    width: 100%;\n    display: inline-flex;\n    justify-content: center;\n    align-items: center;\n    font-size: 2rem;\n    line-height: 2rem;\n    font-weight: bold;\n    vertical-align: middle;\n    box-sizing: border-box;\n    color: var(--tile-text-color);\n    text-transform: uppercase;\n    border-radius:50%;\n    user-select: none;\n  }\n  .tile::before {\n    content: '';\n    display: inline-block;\n    padding-bottom: 100%;\n  }\n\n  /* Allow tiles to be smaller on small screens */\n  @media (max-height: 600px) {\n    .tile {\n      font-size: 1em;\n      line-height: 1em;\n    }\n  }\n\n  .tile[data-state='empty'] {\n    border: 2px solid var(--color-tone-4);\n  }\n  .tile[data-state='tbd'] {\n    background-color: var(--color-tone-7);\n    border: 2px solid var(--color-tone-3);\n    color: var(--color-tone-1);\n  }\n  .tile[data-state='correct'] {\n    background-color: var(--color-correct);\n  }\n  .tile[data-state='present'] {\n    background-color: var(--color-present);\n  }\n  .tile[data-state='absent'] {\n    background-color: var(--color-absent);\n  }\n\n  .tile[data-animation='pop'] {\n    animation-name: PopIn;\n    animation-duration: 100ms;\n  }\n\n  @keyframes PopIn {\n    from {\n      transform: scale(0.8);\n      opacity: 0;\n    }\n\n    40% {\n      transform: scale(1.1);\n      opacity: 1;\n    }\n  }\n  .tile[data-animation='flip-in'] {\n    animation-name: FlipIn;\n    animation-duration: 250ms;\n    animation-timing-function: ease-in;\n  }\n  @keyframes FlipIn {\n    0% {\n      transform: rotateX(0);\n    }\n    100% {\n      transform: rotateX(-90deg);\n    }\n  }\n  .tile[data-animation='flip-out'] {\n    animation-name: FlipOut;\n    animation-duration: 250ms;\n    animation-timing-function: ease-in;\n  }\n  @keyframes FlipOut {\n    0% {\n      transform: rotateX(-90deg);\n    }\n    100% {\n      transform: rotateX(0);\n    }\n  }\n</style>\n<div class=\"tile\" data-state=\"empty\" data-animation=\"idle\"></div>\n";
     var v = (function (e) {
       r(t, e);
       var a = h(t);
@@ -989,6 +986,7 @@
     }	
 //Moved array for random letter words - end. 	
     customElements.define("game-tile", v);
+    var k = document.createElement("template");
     var w = document.createElement("template");
 	var todaysdate = new Date();
           if (window.localStorage.getItem("archiveDate") == null) {
@@ -996,6 +994,17 @@
             window.localStorage.setItem("archiveDate", JSON.stringify(new Date(todaysdate.setDate(todaysdate.getDate() - todayGame + 1))));
           } 
 	var wordlength = Da(new Date(JSON.parse(window.localStorage.getItem("archiveDate")))).length;
+	if (wordlength <= 5){
+		var borderradius = 50;
+	}
+	else if (wordlength == 6){
+		var borderradius = 25;
+	}	
+	else if (wordlength >= 7){
+		var borderradius = 0;
+	}	
+    k.innerHTML =
+      "\n<style>\n  :host {\n    display: inline-block;\n  }\n  .tile {\n    width: 100%;\n    display: inline-flex;\n    justify-content: center;\n    align-items: center;\n    font-size: 2rem;\n    line-height: 2rem;\n    font-weight: bold;\n    vertical-align: middle;\n    box-sizing: border-box;\n    color: var(--tile-text-color);\n    text-transform: uppercase;\n    border-radius:"+borderradius+"%;\n    user-select: none;\n  }\n  .tile::before {\n    content: '';\n    display: inline-block;\n    padding-bottom: 100%;\n  }\n\n  /* Allow tiles to be smaller on small screens */\n  @media (max-height: 600px) {\n    .tile {\n      font-size: 1em;\n      line-height: 1em;\n    }\n  }\n\n  .tile[data-state='empty'] {\n    border: 2px solid var(--color-tone-4);\n  }\n  .tile[data-state='tbd'] {\n    background-color: var(--color-tone-7);\n    border: 2px solid var(--color-tone-3);\n    color: var(--color-tone-1);\n  }\n  .tile[data-state='correct'] {\n    background-color: var(--color-correct);\n  }\n  .tile[data-state='present'] {\n    background-color: var(--color-present);\n  }\n  .tile[data-state='absent'] {\n    background-color: var(--color-absent);\n  }\n\n  .tile[data-animation='pop'] {\n    animation-name: PopIn;\n    animation-duration: 100ms;\n  }\n\n  @keyframes PopIn {\n    from {\n      transform: scale(0.8);\n      opacity: 0;\n    }\n\n    40% {\n      transform: scale(1.1);\n      opacity: 1;\n    }\n  }\n  .tile[data-animation='flip-in'] {\n    animation-name: FlipIn;\n    animation-duration: 250ms;\n    animation-timing-function: ease-in;\n  }\n  @keyframes FlipIn {\n    0% {\n      transform: rotateX(0);\n    }\n    100% {\n      transform: rotateX(-90deg);\n    }\n  }\n  .tile[data-animation='flip-out'] {\n    animation-name: FlipOut;\n    animation-duration: 250ms;\n    animation-timing-function: ease-in;\n  }\n  @keyframes FlipOut {\n    0% {\n      transform: rotateX(-90deg);\n    }\n    100% {\n      transform: rotateX(0);\n    }\n  }\n</style>\n<div class=\"tile\" data-state=\"empty\" data-animation=\"idle\"></div>\n";	
     w.innerHTML =
       '\n  <style>\n    :host {\n      display: block;\n    }\n    :host([invalid]){\n      animation-name: Shake;\n      animation-duration: 600ms;\n    }\n    .row {\n      display: grid;\n      grid-template-columns: repeat('+wordlength+', 1fr);\n      grid-gap: 5px;\n    }\n    .win {\n      animation-name: Bounce;\n      animation-duration: 1000ms;\n    }\n\n    @keyframes Bounce {\n      0%, 20% {\n        transform: translateY(0);\n      }\n      40% {\n        transform: translateY(-30px);\n      }\n      50% {\n        transform: translateY(5px);\n      }\n      60% {\n        transform: translateY(-15px);\n      }\n      80% {\n        transform: translateY(2px);\n      }\n      100% {\n        transform: translateY(0);\n      }\n    }\n\n    @keyframes Shake {\n      10%,\n      90% {\n        transform: translateX(-1px);\n      }\n\n      20%,\n      80% {\n        transform: translateX(2px);\n      }\n\n      30%,\n      50%,\n      70% {\n        transform: translateX(-4px);\n      }\n\n      40%,\n      60% {\n        transform: translateX(4px);\n      }\n    }\n  </style>\n  <div class="row"></div>\n';
     var x = (function (e) {
@@ -1887,6 +1896,8 @@
       var a = za();
       !(function (e) {
         window.localStorage.setItem(wa, JSON.stringify(e));
+		e.lastPlayedTs = Date.now();
+		saveArchive(e);		
       })(va(a, e));
     }
     var Sa = document.createElement("template");
@@ -2080,6 +2091,7 @@
 	  if (gameApp.rowIndex >= 0 && gameApp.boardState[gameApp.rowIndex]){
 		  gameApp.boardState[gameApp.rowIndex] = "";
 	  }  
+	  
       var saveData = {
         rowIndex: gameApp.rowIndex,
         boardState: gameApp.boardState,
@@ -2092,7 +2104,7 @@
     function saveArchiveNoApp() {
       var currentGame = Ga(new Date(JSON.parse(window.localStorage.getItem(archiveDate)))) - 265;
       var gameState = window.localStorage.getItem("archive-gameState");
-      
+     
       if (gameState != null) {
         var gameData = JSON.parse(gameState);
         var saveData = {
@@ -2174,7 +2186,7 @@
           } 
           var currentGame = Ga(new Date(JSON.parse(window.localStorage.getItem(archiveDate)))) - 265;
           var savedData = JSON.parse(window.localStorage.getItem("archive"+JSON.stringify(currentGame)));
-          
+		          
           if (savedData != null) {
             e.lastPlayedTs = savedData.lastPlayedTs;
             ja(savedData);
@@ -2214,7 +2226,7 @@
           o(t, [
             {
               key: "evaluateRow",
-              value: function () {
+              value: function () { 
                 if (wordlength === this.tileIndex && !(this.rowIndex >= 6)) {
                   var e,
                     a = this.$board.querySelectorAll("game-row")[this.rowIndex],
@@ -2483,7 +2495,7 @@
                     saveArchive(e);
                     
                     var currentArchiveDate = new Date(JSON.parse(window.localStorage.getItem(archiveDate)));
-                    window.localStorage.setItem(archiveDate, JSON.stringify(new Date(currentArchiveDate.setDate(currentArchiveDate.getDate() + 1))));
+                    window.localStorage.setItem(archiveDate, JSON.stringify(new Date(currentArchiveDate.setDate(currentArchiveDate.getDate() + 1)))); 
                     
                     window.location.reload();
                 })),
@@ -3378,7 +3390,7 @@
                   //       );
                   //     });
                 var announcement = document.createElement("div");
-                announcement.innerHTML = '<style> a {color: var(--orange);} a:hover {color: var(--green);}</style> <center style="font-size:12px", "acolor: #FF0000">Check out <a href="https://www.twitter.com/IndleGame" target="_blank">@IndleGame</a> on Twitter!</center>';
+                announcement.innerHTML = '<style> a {color: var(--orange);} a:hover {color: var(--green);}</style> <center style="font-size:12px", "acolor: #FF0000">Follow us <a href="https://www.twitter.com/IndleGame" target="_blank"><strong>@IndleGame</strong></a> on <strong>Twitter!<strong></center>';
                 this.shadowRoot.appendChild(announcement); 					  
                 }
               },
